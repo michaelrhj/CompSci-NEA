0e2820b38f6c4c49a99542b97227d6b536fd0de6
michaelrhj <mic_471@yahoo.com>
2020-12-18 22:36:22 +0000

    18/12/2020

--- project.py
+++ project.py
@@ -1,6 +1,7 @@
 #importing libraries that are used
 import sys
 from tkinter import *
+import sqlite3
 
 #main menu screen
 def home():
@@ -25,14 +26,52 @@ def home():
 	loginButton = Button(window,text="Login",width="8").place(x=10, y=200)
 
 	#register labels and entries
+	global registerUsernameEntry
+	global registerPasswordEntry
 	registerUsernameLabel = Label(window,bg="#0394fc",text="Username: ").place(x=500, y=120)
 	registerUsernameEntry = Entry(window).place(x=570, y=120)
 	registerPasswordLabel = Label(window,bg="#0394fc",text="Password: ").place(x=500, y=160)
 	registerPasswordEntry = Entry(window).place(x=570, y=160)	
 
 	#register button
-	registerButton = Button(window,text="Register",width="8").place(x=500, y=200)
+	registerButton = Button(window,text="Register",width="8",command=registerUser).place(x=500, y=200)
 
 	window.mainloop()
 
+def registerUser():
+	username = registerUsernameEntry.get()
+	password = registerPasswordEntry.get()
+	print(username,password)
+
+def addUser():
+	username = "master"
+	password = "master"
+
+	conn = sqlite3.connect('ProjectileMotionGame.db')
+	c = conn.cursor()	
+	c.execute("INSERT INTO users VALUES (?,?)",(username,password))
+	conn.commit()
+	conn.close()
+
+
+#procedure to create table in my database
+def createTable():
+	conn = sqlite3.connect('ProjectileMotionGame.db') #connects to database
+	c = conn.cursor() #cursor class allows you to invoke methods that execute sqlite statements
+	#table is created with username and password fields
+	c.execute("""CREATE TABLE users(
+		username text,
+		password text
+		)""")
+	conn.commit()
+	conn.close()
+
+
+#to create the database
+def createDatabase():
+	conn = sqlite3.connect(r"C:\Users\lola_\Desktop\Computer Science NEA\ProjectileMotionGame.db")
+	c = conn.cursor()
+
+
 home()
+
